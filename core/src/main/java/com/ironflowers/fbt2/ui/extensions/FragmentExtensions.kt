ec5a5d8c9e20c3ac83ac8fbe72f4a272907a511c
frank <frankf@q42.nl>
2019-03-06 15:27:14 +0100

    CHANGE added requireActivity

--- core/src/main/java/com/ironflowers/fbt2/ui/extensions/FragmentExtensions.kt
+++ core/src/main/java/com/ironflowers/fbt2/ui/extensions/FragmentExtensions.kt
@@ -3,4 +3,4 @@ package com.ironflowers.fbt2.ui.base
 import androidx.fragment.app.Fragment
 import com.ironflowers.fbt2.ui.extensions.coreComponent
 
-fun Fragment.coreComponent() = activity!!.coreComponent()
+fun Fragment.coreComponent() = requireActivity().coreComponent()
